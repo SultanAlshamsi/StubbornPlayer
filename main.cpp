8a4454f179b7e4dd49517dea343fe77cf903534b
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-03-01 20:13:30 -0800

    Stubborn

--- main.cpp
+++ main.cpp
@@ -7,5 +7,39 @@ int main()
 {
   SecretDoor game;
     
+
+    int realnumber;
+    int victory;
+
+
+
+
+   cout<<"How many times do you want to play?\n";
+
+   cin>>realnumber;
+
+
+    for(int index=0; index<realnumber; index++)
+    {
+
+        game.newGame();
+        game.guessDoorC();
+        game.guessDoorC();
+
+ if(game.isWinner()==true)
+
+ {
+
+ victory++;
+ }
+ }
+
+ cout<<"You have won "<<victory<<endl;
+
+
+
+
+
+
   return 0;
 }
